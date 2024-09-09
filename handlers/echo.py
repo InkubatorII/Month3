8cadb22cb9fba14d6aac502c0169b169ea24bde6
InkubatorII <holloward@gmail.com>
2024-09-09 21:10:48 +0600

    hw2

--- handlers/echo.py
+++ handlers/echo.py
@@ -1,8 +1,38 @@
+import random
+from config import bot
 from aiogram import types, Dispatcher
-
+import asyncio
+games=['⚽', '🎰', '🏀', '🎯', '🎳', '🎲']
 # Эхо - отвечает тем же текстом
 async def echo_handler(message: types.Message):
     await message.answer(message.text)
+    text = message.text
+
+    if text.isdigit():
+        await message.answer(int(text) ** 2)
+
+    elif text == 'game':
+        bot_dice = await bot.send_dice(
+            chat_id=message.from_user.id,
+            emoji=random.choice(games)
+        )
+        user_dice = await bot.send_dice(
+            chat_id=message.from_user.id,
+            emoji=random.choice(games)
+        )
+        await bot.send_message(chat_id=message.from_user.id, text="Подождите, идет бросок...")
+        await asyncio.sleep(4)  # Ждем завершения анимации (примерно 4 секунды)
+
+        if bot_dice.dice.value > user_dice.dice.value:
+            await bot.send_message(chat_id=message.from_user.id, text="Бот победил!")
+        elif bot_dice.dice.value < user_dice.dice.value:
+            await bot.send_message(chat_id=message.from_user.id, text="Вы победили!")
+        else:
+            await bot.send_message(chat_id=message.from_user.id, text="Ничья!")
+
+    else:
+        await message.answer(text)
+
 
 def register_echo(dp: Dispatcher):
     dp.register_message_handler(echo_handler)
--- handlers/quiz.py
+++ handlers/quiz.py
@@ -5,8 +5,7 @@ from config import bot
 
 async def quiz_1(message: types.Message):
     quiz_button = InlineKeyboardMarkup()
-    button_qyuz_1 = InlineKeyboardButton('Дальше...',
-                                         callback_data='button_1')
+    button_qyuz_1 = InlineKeyboardButton(text='Дальше...', callback_data='button_1')
     quiz_button.add(button_qyuz_1)
 
     question = 'BMW or Mercedes?'
@@ -26,6 +25,9 @@ async def quiz_1(message: types.Message):
 
 
 async def quiz_2(call: types.CallbackQuery):
+    quiz_button2 = InlineKeyboardMarkup()
+    button_qyuz_2 = InlineKeyboardButton(text='Дальше...', callback_data='button_2')
+    quiz_button2.add(button_qyuz_2)
 
     question = "Frontend or Backend"
     answer = ['Frontend', 'Backend', 'IOS']
@@ -38,10 +40,29 @@ async def quiz_2(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=1,
         explanation='Damn -_-',
-        open_period=60
+        open_period=60,
+        reply_markup=quiz_button2
+    )
+
+async def quiz_3(call: types.CallbackQuery):
+
+    question = 'Кто победит на The International 2024?'
+    answer = ['Team Spirit', 'Team Liquid', 'Falcons', 'Gaming Gladiators']
+
+    await bot.send_poll(
+        chat_id=call.from_user.id,
+        question=question,
+        options=answer,
+        is_anonymous=True,
+        type='quiz',
+        correct_option_id=0,
+        explanation='Team Spirit  - Балда',
+        open_period=60,
+
     )
 
 
 def register_quiz(dp: Dispatcher):
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_callback_query_handler(quiz_2, text='button_1')
+    dp.register_callback_query_handler(quiz_3, text='button_2')
