fec68d91bc5e3a09e7c257a6284d1e192115c290
InkubatorII <holloward@gmail.com>
2024-09-17 17:32:03 +0600

    lesson4

--- buttons.py
+++ buttons.py
@@ -8,7 +8,9 @@ mem_buttons = KeyboardButton('/mem')
 mem_all_buttons = KeyboardButton('/mem_all')
 music_buttons = KeyboardButton('/music')
 
-start.add(start_buttons, mem_buttons, mem_all_buttons, music_buttons)
+
+start.add(start_buttons, mem_buttons, mem_all_buttons,
+          music_buttons)
 
 #2 способ добавления кнопок
 
@@ -27,6 +29,12 @@ size_product_keyboard.add(
                   KeyboardButton('XL'),
                   KeyboardButton('XXL')
 )
+
+cancel_button = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Отмена'))
+
+submit_button = ReplyKeyboardMarkup(resize_keyboard=True,
+                                    row_width=2).add(KeyboardButton('Да'), KeyboardButton('Нет'))
+
 #
 # #3 способ
 # start_test_1 = ReplyKeyboardMarkup(
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from aiogram.utils import executor
 from buttons import start_test
 from config import bot, dp, admin
 from handlers import commands, echo, quiz, FSM_registration, FSM_store
-
+from db import db_main
 
 async def on_startup(_):
     for i in admin:
-        await bot.send_message(chat_id=i, text='Бот включен!',
+        await bot.send_message(chat_id=i, text="Бот включен!",
                                reply_markup=start_test)
+        await db_main.sql_create()
+
 
 
 commands.register_commands(dp)
 quiz.register_quiz(dp)
 FSM_registration.register_fsm_reg(dp)
-FSM_store.register_fsm_reg_store(dp)
+FSM_store.register_store(dp)
 
 
 
